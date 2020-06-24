191f84403ebe2486c2b49e9e323be72c44170b9d
Brian Fajardo <bmf5@hotmail.com>
2020-06-24 12:58:59 -0400

    Create UITableViewController with scaffolded rows

--- slideoutMenu/ViewController.swift
+++ slideoutMenu/ViewController.swift
@@ -8,10 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupNavigationItems()
+        tableView.backgroundColor = UIColor.red
+    }
+    
+    @objc func handleOpen() {
+        print("opening menu")
+    }
+    
+    @objc func handleHide() {
+        print("hiding menu")
+    }
+    
+    fileprivate func setupNavigationItems() {
+        navigationItem.title = "Home"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellId")
+        
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        
+        return cell
     }
 }
 
