bdf97e4b0448b075ea36c2cb6adf5121a7d9b16e
Brian Fajardo <bmf5@hotmail.com>
2020-06-24 12:28:17 -0400

    Initial commit

---
+++ slideoutMenu/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  slideoutMenu
+//
+//  Created by Brian Fajardo on 6/22/20.
+//  Copyright © 2020 Brian Fajardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
