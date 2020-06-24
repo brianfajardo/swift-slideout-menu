7370450a6281edad62023d6dc2b1ee4f00cc20b7
Brian Fajardo <bmf5@hotmail.com>
2020-06-24 12:29:16 -0400

    Bypass Storyboard in AppDelegate

--- slideoutMenu/AppDelegate.swift
+++ slideoutMenu/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        // Bypass the Storyboard
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        
         return true
     }
 
