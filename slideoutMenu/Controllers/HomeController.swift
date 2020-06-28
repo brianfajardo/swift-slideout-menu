f926cc099869234771604d65a5dc5abd05dcd623
Brian Fajardo <bmf5@hotmail.com>
2020-06-28 12:51:37 -0400

    Refactor

--- slideoutMenu/Controllers/HomeController.swift
+++ slideoutMenu/Controllers/HomeController.swift
@@ -16,41 +16,44 @@ class HomeController: UITableViewController {
         super.viewDidLoad()
         
         setupNavigationItems()
+        setupMenuController()
+        
         tableView.backgroundColor = UIColor.red
     }
     
     @objc func handleOpen() {
-        let mainWindow = UIApplication.shared.keyWindow
-
-        // Setup initial position of menu off-screen
-        menuController.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: self.view.frame.height)
-        
-        mainWindow?.addSubview(menuController.view)
-        
-        // Animate menu from the left into users' view
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            self.menuController.view.transform = CGAffineTransform(translationX: self.menuWidth, y: 0)
-        })
-        
-        addChild(menuController)
+        performAnimations(withTransformTo: CGAffineTransform(translationX: self.menuWidth, y: 0))
     }
     
     @objc func handleHide() {
-        // Animate menu to the left to hide from users' view
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            self.menuController.view.transform = .identity
-        })
-        
-//        menuController.view.removeFromSuperview()
-//        menuController.removeFromParent()
+        performAnimations(withTransformTo: .identity)
     }
     
+    // MARK:- Fileprivate
+    
     fileprivate let menuWidth: CGFloat = 300
+    
+    fileprivate func performAnimations(withTransformTo transformation: CGAffineTransform) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.menuController.view.transform = transformation
+        })
+    }
     
     fileprivate func setupNavigationItems() {
         navigationItem.title = "Home"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
+    }
+    
+    fileprivate func setupMenuController() {
+        let mainWindow = UIApplication.shared.keyWindow
+        
+        // Setup initial position of menu off-screen
+        menuController.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: self.view.frame.height)
+        
+        mainWindow?.addSubview(menuController.view)
+        
+        addChild(menuController)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
